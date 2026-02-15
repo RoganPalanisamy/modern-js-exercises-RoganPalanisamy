b4f05fa536ea4eedb8948341e6536f4cabd8aad3
Rogan Palanisamy <p.roganpalanisamy1012@gmail.com>
2026-02-14 18:51:51 -0700

    Update ex01.js

--- exercises/ex01.js
+++ exercises/ex01.js
@@ -7,8 +7,8 @@ Create a function named sumLargestNumbers that will receive an array of numbers 
 */
 
 const sumLargestNumbers = function (data) {
-  // Put your solution here
-  return 11;
+  data.sort((a, b) => b - a);
+  return data[0] + data[1];
 };
 
 console.log(sumLargestNumbers([1, 10])); // 11
